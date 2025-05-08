4f6f5e7e895c6a387dd093eb7f8af48eff79b712
Gaßmann, Aaron <Aaron.Gassmann@dts.de>
2025-05-08 14:26:22 +0200

    Refactor CSS styles and update App component to display percentage values for possibilities

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,31 +13,31 @@ function App() {
           {/* Display Android/Apple possibilities */}
           <div className="card-item">
             <span className="item-key">Android:</span>
-            <span className="item-value">{possibilities.an}</span>
+            <span className="item-value">{possibilities.an}%</span>
           </div>
            <div className="card-item">
             <span className="item-key">Apple:</span>
-            <span className="item-value">{possibilities.ap}</span>
+            <span className="item-value">{possibilities.ap}%</span>
           </div>
 
           {/* Display Age possibilities */}
           <div className="card-item">
             <span className="item-key">Young:</span>
-            <span className="item-value">{possibilities.y}</span>
+            <span className="item-value">{possibilities.y}%</span>
           </div>
           <div className="card-item">
             <span className="item-key">Old:</span>
-            <span className="item-value">{possibilities.o}</span>
+            <span className="item-value">{possibilities.o}%</span>
           </div>
 
           {/* Display Wealth possibilities */}
            <div className="card-item">
             <span className="item-key">Rich:</span>
-            <span className="item-value">{possibilities.r}</span>
+            <span className="item-value">{possibilities.r}%</span>
           </div>
           <div className="card-item">
             <span className="item-key">Poor:</span>
-            <span className="item-value">{possibilities.p}</span>
+            <span className="item-value">{possibilities.p}%</span>
           </div>
 
         </div>
